f7ddddcc99ed9129720d2b335d9182156211f396
Gerson2102 <gersonloavas@gmail.com>
2024-11-27 15:00:42 -0600

    Adding subscription service implementation

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,11 +1,14 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, routing::post, Router};
 mod health_check;
 mod types;
+mod subscription;
 
 use crate::AppState;
 
 // Application router.
 // All routes should be merged here.
 pub fn router() -> Router<AppState> {
-    Router::new().route("/health_check", get(health_check::health_check))
+    Router::new()
+        .route("/health_check", get(health_check::health_check))
+        .route("/subscriptions", post(subscription::create_subscription))
 }
